fd809a3e4e8cfebe8d948b9d0921cc25b22ed8c9
SwampDiver <toashes88@gmail.com>
2021-06-10 21:20:08 +0700

    скрипты для swiper slider

--- app/js/swiper-slider.js
+++ app/js/swiper-slider.js
@@ -1,1 +1,19 @@
+// --- SLIDER ---
 
+new Swiper('.slider-banner-main__container', {
+    navigation: {
+        nextEl: '.banner-main__slider-toggle--next',
+        prevEl: '.banner-main__slider-toggle--prev'
+    },
+    scrollbar: {
+        el: '.slider-banner-main__scrollbar',
+        draggable: true
+    },
+
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+    },
+
+    speed: 800,
+});
