11358af778138d99618af5e8182350addba3106a
SwampDiver <72814211+SwampDiver@users.noreply.github.com>
2021-07-08 13:45:06 +0700

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,1 @@
+const menuBtn=document.querySelector(".icon-menu"),menu=document.querySelector(".nav-header");menuBtn.addEventListener("click",e=>{e.preventDefault(),menuBtn.classList.toggle("icon-menu--active"),menu.classList.toggle("nav-header--active"),document.body.classList.toggle("lock")});
---
+++ js/swiper-slider.js
@@ -0,0 +1,1 @@
+new Swiper(".slider-banner-main__container",{navigation:{nextEl:".banner-main__slider-toggle--next",prevEl:".banner-main__slider-toggle--prev"},scrollbar:{el:".slider-banner-main__scrollbar",draggable:!0},autoplay:{delay:2e3,disableOnInteraction:!1},speed:800});
