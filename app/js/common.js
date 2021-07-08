ed08c160be9cf4980fa6d2bf8b866e3d8749b3c4
SwampDiver <72814211+SwampDiver@users.noreply.github.com>
2021-07-08 13:39:05 +0700

    Delete app/js directory

--- app/js/common.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.querySelector(".icon-menu"),
-    menu = document.querySelector(".nav-header");
-
-menuBtn.addEventListener("click", e => {
-    e.preventDefault();
-    menuBtn.classList.toggle("icon-menu--active");
-    menu.classList.toggle("nav-header--active");
-    document.body.classList.toggle("lock");
-});
